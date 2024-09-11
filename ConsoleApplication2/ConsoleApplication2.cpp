3c076421d3bc54cdfe23ff8d8a6d5dcb6e9264f4
kbstudent <kbstudent@KB37-309-C06>
2024-09-11 11:34:54 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+int integ = 25;
+int teg;
+
+int main()
+{
+	std:: cout << "sdsfsf";
+	wchar_t i1{ L'i' };
+	std:: wcout << i1 << '\n';
+	int c12 = 25;
+	char w [] = "west";
+	teg = 55;
+	string w1 = "lady";
+	int w4 = 20;
+	string s1 = "hello";
+	char ch1 [] = "sdewqe,4325";
+	double d1 = 0.21;
+	float f1 = 25.6;
+	int a1, b1, c1;
+	a1 = 5;
+	b1 = 4;
+	c1 = 32;
+	char a9 [] = "A,5623";
+	int a5 = 25;
+	char a55 [] = "H4325,532";
+	string str = "Hello";
+	char b8 [] = "B325,325523";
+	int a12, a33, c3;
+	a12 = 444;
+	a33 = 4124;
+	c3 = 321412;
+	char s1 = 'i';
+	char go [] = "lets,go";
+	int inerg = 22; 
+	cout << integ << endl<< integ <<endl ;
+	return 0;
+}
+//отладка кода готова 
